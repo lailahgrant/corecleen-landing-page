f9f25c98b3b37b66f79f8cb491b710885eb58728
lailahgrant <lailahgrant19@gmail.com>
2025-06-24 15:49:10 +0300

    added a scroll to the top button

--- js/script.js
+++ js/script.js
@@ -20,6 +20,29 @@ const headerBg = () => {
 // call the function
 headerBg();
 
+/**
+ * SCROLL TO TOP BUTTON
+ */
+// Get the button
+let myscrollbutton = document.getElementById("myScrollTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myscrollbutton.style.display = "block";
+  } else {
+    myscrollbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 
 /**
  * MODAL
